3ec796f6b251fcb7d66d2d19716ef861b2d62a07
Tahjw <Tahjwill2@gmail.com>
2016-01-22 14:42:17 -0600

    I added my first arduino sketch may the force be with you in morse code

---
+++ sketch_Myfirst.ino
@@ -0,0 +1,255 @@
+void setup() {
+  //we want to see if pin 13 is installed by default
+  pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // blink LED light in morse code say: May the force be with you
+ 
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);  
+  digitalWrite(13,LOW);//M
+  delay(3000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);//A
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH); 
+  delay(3000);
+  digitalWrite(13,LOW); //Y
+
+  
+  delay(7000);//end of word
+
+  
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //T
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //H
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //E
+  
+  delay(7000); //end of word
+
+ 
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //F
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //O
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //R
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //C
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //E
+  
+  delay(7000); //end of word
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000); 
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //B
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //E
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //W
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);//I
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //T
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW); //H
+  
+  delay(7000); //end of word
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH); 
+  delay(3000);
+  digitalWrite(13,LOW); //Y
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //O
+  delay(1000);
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW); //U
+  delay(10000);
+  
+
+  
+
+}
